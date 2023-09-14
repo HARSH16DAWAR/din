dfd6c363c29da6b2025faf6c44c6fbef310623b2
HARSH16DAWAR <hdawar16@gmail.com>
2023-09-14 23:14:10 +0530

    added routes/connections

---
+++ components/Editor.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import { useState } from 'react'
+
+const Editor = ({ entry }) => {
+  const [value, setValue] = useState(entry.content)
+
+  return (
+    <div className="w-full h-full">
+      <textarea
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        className="w-full h-full p-8 text-xl outline-none"
+      ></textarea>
+    </div>
+  )
+}
+
+export default Editor
